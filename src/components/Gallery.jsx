e1e8b9e5a087608dc5c13b3481952df4330d48fd
M.S.T.O.P <mstop4@gmail.com>
2022-07-29 20:34:34 -0400

    switch to thumbnails

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,17 +3,19 @@ import settings from '../data/settings.json';
 
 export const Gallery = (props) => {
   const { photos } = props;
-  const { photosBaseUrl } = settings;
+  const { photosBaseUrl, thumbnailsBaseUrl } = settings;
 
   const photoElems = [];
   for (const photo of photos) {
     photoElems.push(
-      <img
-        className="Gallery-photo"
-        src={`${photosBaseUrl}${photo}`}
-        alt={photo}
-        key={photo}
-      />
+      <a href={`${photosBaseUrl}${photo}`}>
+        <img
+          className="Gallery-photo"
+          src={`${thumbnailsBaseUrl}${photo}`}
+          alt={photo}
+          key={photo}
+        />
+      </a>
     );
   }
 
